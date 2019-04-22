757330ec378da356e9a9dae2e1a5734c58ffdb46
Vitalii <vitalik.bondarqq@gmail.com>
2019-04-22 22:52:29 +0300

    add dnd in a same column

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import TrelloList from "./TrelloList";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import ActionButton from "./ActionButton";
+import {DragDropContext} from "react-beautiful-dnd";
+import {sort} from "../actions";
+import styled from "styled-components";
+
+const ListContainer = styled.div`
+     display: flex;
+     flex-direction: row;
+`;
+
 class App extends Component {
-  render() {
-      const {lists}=this.props;
-    return (
-      <div className="App">
-        <h2>Test</h2>
-          <div style={styles.listContainer}>
-          {lists.map(list=>(<TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards}/>))}
-          <ActionButton list/>
-          </div>
-      </div>
-    );
-  }
+    onDragEnd = (result) => {
+        const {destination, source, draggableId} = result;
+        if (!destination) {
+            return;
+        }
+        this.props.dispatch(
+            sort(
+                source.droppableId,
+                destination.droppableId,
+                source.index,
+                destination.index,
+                draggableId
+            )
+        )
+
+    };
+
+    render() {
+        const {lists} = this.props;
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                <div>
+                    <h2>Test</h2>
+                    <ListContainer>
+                        {lists.map(list => (
+                            <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards}/>))}
+                        <ActionButton list />
+                    </ListContainer>
+                </div>
+            </DragDropContext>
+        );
+    }
 }
 
-const styles = {
-    listContainer:{
-        display:"flex",
-        flexDirection: "row",
-    }
-};
-
 const mapStateToProps = state => ({
-    lists:state.lists
+    lists: state.lists
 });
 export default connect(mapStateToProps)(App);
